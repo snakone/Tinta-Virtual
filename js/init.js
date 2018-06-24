c197c4ad2c9400a38f582a9ba5ac75a3be5cb029
Sergi <snak_one@hotmail.com>
2018-06-24 16:32:15 +0200

    ScrollSpy and Modal

--- js/init.js
+++ js/init.js
@@ -1,20 +1,16 @@
-
-
 (function($){
   $(function(){
-
     $('.sidenav').sidenav();
     $('.parallax').parallax();
 	$('.carousel').carousel();
-   
-  $(document).ready(function () {
-
-            $("a.scrollLink").click(function (event) {
-                event.preventDefault();
-                $("html, body").animate({ scrollTop:$($(this).attr("href")).offset().top }, 1000);
-			$('.sidenav').sidenav('close');
-            });
-        });
+	$('.modal').modal();
+	$('.scrollspy').scrollSpy();
+	
+	$("a.scrollLink").click(function (event) {
+		event.preventDefault();
+		$("html, body").animate({ scrollTop:$($(this).attr("href")).offset().top }, 1000);
+	$('.sidenav').sidenav('close');
+		});
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
