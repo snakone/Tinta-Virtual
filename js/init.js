6e7da897ecb471224f4d670a0b71c3c79686bc25
Sergi <snak_one@hotmail.com>
2018-06-25 09:49:09 +0200

    Top Section as Window Size

--- js/init.js
+++ js/init.js
@@ -11,6 +11,17 @@
 		$("html, body").animate({ scrollTop:$($(this).attr("href")).offset().top }, 1000);
 	$('.sidenav').sidenav('close');
 		});
+		
+	/*Make Top Section Adjust by Window Size */
+	
+	var wheight = $(window).height();
+	$('#index-banner').css('height', wheight);
+	$(window).resize(function(){
+	var wheight = $(window).height();
+	$('#index-banner').css('height', wheight);
+		
+	});
+	
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
